a286162e60f3ad60ae804a9ab37a5ef93a27a956
greg <greg@033d73b4-3e0c-0410-a4cb-82f55ea64545>
2005-01-16 01:40:16 +0000

    add docs
    git-svn-id: file:///Users/greg/svn2/trunk/CornerClick@362 033d73b4-3e0c-0410-a4cb-82f55ea64545

--- transbuild/makenib.pl
+++ transbuild/makenib.pl
@@ -22,4 +22,15 @@ if(system("nibtool -d \"$strings\" -w \"$lang/$NIB.nib\" $IN")){
 	warn "created incremental file: $lang/$NIB.nib\n";
 }
 
+=head1 WHAT
 
+Converts a Nib from the base language (English) to the translated language,
+using the file (NibName).strings inside the lproj of the new language.
+
+E.g.:
+
+perl makenib.pl French
+
+This converts English.lproj/Nib.nib to ./French/Nib.nib using the strings file French.lproj/Nib.strings
+
+=cut

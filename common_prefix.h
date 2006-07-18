df3312f3141827f3a1b0d71ee5545f4569db79a7
greg <greg@033d73b4-3e0c-0410-a4cb-82f55ea64545>
2006-07-18 07:33:25 +0000

    update version, add screen saver action constant definition
    git-svn-id: file:///Users/greg/svn2/trunk/CornerClick@397 033d73b4-3e0c-0410-a4cb-82f55ea64545

--- common_prefix.h
+++ common_prefix.h
@@ -6,7 +6,7 @@
 #define EMAIL_URL @"mailto:greg-cornerclick@vario.us"
 
 
-#define MARKETING_VERSION_STRING @"0.7"
+#define MARKETING_VERSION_STRING @"0.8"
 
 #define SHIFT_MASK (1<<0)
 #define OPTION_MASK (1<<1)
@@ -34,6 +34,7 @@
 #define ACT_EAPP 6
 #define ACT_EDES 7
 #define ACT_DASH 8
+#define ACT_SCRE 9
 
 #define DEBUG_LEVEL 0
 #define DEBUG2(...)  if(DEBUG_LEVEL>3)NSLog(__VA_ARGS__)
@@ -46,10 +47,10 @@
     #define WRITE_BUBBLES
 #endif
 
-#define CC_APP_VERSION 0007
+#define CC_APP_VERSION 8
 #define CC_PATCH_VERSION 0
 
-#define CC_MIN_VERSION 0002
-#define CC_MAX_VERSION 0007
+#define CC_MIN_VERSION 2
+#define CC_MAX_VERSION 8
 
 #define LOCALIZE(x,y)	[x localizedStringForKey:y value:nil table:nil]
